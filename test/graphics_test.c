4fdeef0b9172ee8413407b196cd4fe250aad367f
bmli <bmli@pge.com>
2020-01-23 01:54:37 +0000

    added berserker coin

--- test/graphics_test.c
+++ test/graphics_test.c
@@ -6,25 +6,31 @@
 
 int main(int argc, char *argv[])
 {
-	Bitmap board, bag_bird;
-	Bitmap gcm;
+	Bitmap board, gcm, ac, bc;
 	char s;
 	Win win;
 	Pos pos;
 	Pix ind;
-	int x, y;
+	int x, y, maxx, maxy;
 
-	load_bitmap(&gcm, "./res/gold_control_marker.bmp");
-	load_bitmap(&board, "./res/board.bmp");
-	load_bitmap(&bag_bird, "./res/bag_bird.bmp");
+	x = 600 - 78 - 294;
+	y = 0;
+	maxx = 600;
+	maxy = 360;
+
 	ind.r = 0;
 	ind.g = 255;
 	ind.b = 0;
+
+	load_bitmap(&gcm, "./res/gold_control_marker.bmp");
+	load_bitmap(&board, "./res/board.bmp");
+	load_bitmap(&ac, "./res/archer_coin.bmp");
+	load_bitmap(&bc, "./res/beserker_coin.bmp");
 	set_ind(&gcm, &ind);
+	set_ind(&ac, &ind);
+	set_ind(&bc, &ind);
 	init_win(&win);
 	show_scr();
-	x = 306;
-	y = 0;
 	pos.x = x;
 	pos.y = y;
 	draw_bitmap(&board, &win, &pos);
@@ -34,9 +40,12 @@ int main(int argc, char *argv[])
 	pos.x = x + 50;
 	pos.y = y + 253;
 	draw_bitmap(&gcm, &win, &pos);
-	pos.x = 2;
-	pos.y = 246;
-	draw_bitmap(&bag_bird, &win, &pos);
+	pos.x = maxx - 2 - 36;
+	pos.y = maxy - 2 - 36 ;
+	draw_bitmap(&ac, &win, &pos);
+	pos.x = maxx - 2 - 36 - 2 - 36;
+	pos.y = maxy - 2 - 36;
+	draw_bitmap(&bc, &win, &pos);
 	scanf("%1s", &s);
 	close_scr();
 
